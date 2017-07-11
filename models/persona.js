a42be6dab3b47b81df891facc1dc9f1f9adc694c
Javier Arpa Robig <javierarpa@gmail.com>
2017-07-11 16:20:00 +0200

    - add: model persona

---
+++ models/persona.js
@@ -0,0 +1,17 @@
+/* 
+ * Modelo de datos de Ejemplo
+ * @author Javier Arpa
+*/
+
+var mongoose = require('mongoose'),  
+    Schema   = mongoose.Schema;
+
+var personaSchema = new Schema({  
+  nombre   : {type: String}, // Nombre
+  apellido : {type: String}, // Apellido
+  edad     : {type:String},  // Edad
+  cDate    : {type: Date,    // fecha de creación
+           default: Date.now} 
+});
+
+module.exports = personaSchema; 
